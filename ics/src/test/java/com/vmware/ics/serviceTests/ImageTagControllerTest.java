08c80f21e15186edadaef40ce2ed0c7c159e6d4a
Danail Nenkov <dpnenkov29@abv.bg>
2023-06-02 19:20:42 +0300

    removed anotation

--- ics/src/test/java/com/vmware/ics/serviceTests/ImageTagControllerTest.java
+++ ics/src/test/java/com/vmware/ics/serviceTests/ImageTagControllerTest.java
@@ -17,7 +17,7 @@ import org.springframework.http.ResponseEntity;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+
 @ExtendWith(MockitoExtension.class)
 public class ImageTagControllerTest {
 
